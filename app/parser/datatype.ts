3776820b62fa8c974634c0e024c529160f34d2cc
Evgenii Perminov <evperminov@ozon.ru>
2025-04-07 02:15:41 +0400

    implement resp integer

--- app/parser/datatype.ts
+++ app/parser/datatype.ts
@@ -38,3 +38,28 @@ export const SimpleError = Schema.TemplateLiteralParser(
 		strict: true,
 	}),
 );
+
+const IntegerPrefix = ":";
+export const Integer = Schema.TemplateLiteralParser(
+	IntegerPrefix,
+	Schema.Literal("+", "-", ""),
+	Schema.String.pipe(Schema.pattern(/^\d+$/), Schema.parseNumber, Schema.int()),
+	CRLF,
+).pipe(
+	Schema.transform(Schema.Number, {
+		decode(input) {
+			const number = input[2];
+			const sign = input[1] === "-" ? -1 : 1;
+			return sign * number;
+		},
+		encode(input) {
+			return [
+				IntegerPrefix,
+				input < 0 ? "-" : "",
+				Math.abs(input),
+				CRLF,
+			] as const;
+		},
+		strict: true,
+	}),
+);
