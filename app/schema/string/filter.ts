0418c23a17759aadd429c901a7a56ba7b37d4a49
Evgenii Perminov <evperminov@ozon.ru>
2025-04-06 12:55:19 +0400

    add schema string filters for not pattern and not includes

---
+++ app/schema/string/filter.ts
@@ -0,0 +1,27 @@
+import { Schema } from "effect";
+
+export function notIncludes<I extends string, A, R>(search: string) {
+	const formatted = JSON.stringify(search);
+	return Schema.filter<Schema.Schema<I, A, R>>(
+		(value) => {
+			return !value.includes(search);
+		},
+		{
+			title: `not includes(${formatted})`,
+			description: `a string not including ${formatted}`,
+		},
+	);
+}
+
+export function notPattern<I extends string, A, R>(regex: RegExp) {
+	const source = regex.source;
+	return Schema.filter<Schema.Schema<I, A, R>>(
+		(value) => {
+			regex.lastIndex = 0;
+			return !regex.test(value);
+		},
+		{
+			description: `a string not matching the pattern ${source}`,
+		},
+	);
+}
