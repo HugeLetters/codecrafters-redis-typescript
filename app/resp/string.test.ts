aabf06557ca5a52c4ab5641aa852a778402dad9c
Evgenii Perminov <evperminov@ozon.ru>
2025-04-12 00:35:46 +0400

    simple error tests

--- app/resp/string.test.ts
+++ app/resp/string.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "@effect/vitest";
 import { Effect, Schema } from "effect";
-import { SimpleString } from "./string";
+import { SimpleError, SimpleErrorFromString, SimpleString } from "./string";
 
 describe("SimpleString", () => {
 	const decode = Schema.decodeUnknown(SimpleString);
@@ -43,6 +43,53 @@ describe("SimpleString", () => {
 		it.effect("is not decoded when contains \\n", ({ expect }) => {
 			return Effect.gen(function* () {
 				const result = yield* decode("+O\rK\r\n").pipe(Effect.isFailure);
+				expect(result).toBe(true);
+			});
+		});
+	});
+});
+
+describe("SimpleErrorFromString", () => {
+	const decode = Schema.decodeUnknown(SimpleErrorFromString);
+	const encode = Schema.encodeUnknown(SimpleErrorFromString);
+
+	describe("with valid data", () => {
+		const EncodedError = "-Error message\r\n";
+		const DecodedError = new SimpleError({ message: "Error message" });
+
+		it.effect("is decoded", ({ expect }) => {
+			return Effect.gen(function* () {
+				const result = yield* decode(EncodedError);
+				expect(result).toEqual(DecodedError);
+			});
+		});
+
+		it.effect("is encoded", ({ expect }) => {
+			return Effect.gen(function* () {
+				const result = yield* encode(DecodedError);
+				expect(result).toBe(EncodedError);
+			});
+		});
+	});
+
+	describe("with invalid data", () => {
+		it.effect("is not decoded when doesnt conform to schema", ({ expect }) => {
+			return Effect.gen(function* () {
+				const result = yield* decode("invalid").pipe(Effect.isFailure);
+				expect(result).toBe(true);
+			});
+		});
+
+		it.effect("is not decoded when contains \\r", ({ expect }) => {
+			return Effect.gen(function* () {
+				const result = yield* decode("-err\nor\r\n").pipe(Effect.isFailure);
+				expect(result).toBe(true);
+			});
+		});
+
+		it.effect("is not decoded when contains \\n", ({ expect }) => {
+			return Effect.gen(function* () {
+				const result = yield* decode("-err\ror\r\n").pipe(Effect.isFailure);
 				expect(result).toBe(true);
 			});
 		});
