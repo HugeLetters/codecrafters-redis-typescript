43ec25fbdf859c453bebaa6c40f5792e85cafb64
Evgenii Perminov <evperminov@ozon.ru>
2025-04-11 21:41:24 +0400

    use schema compose for int from str

--- app/config.ts
+++ app/config.ts
@@ -1,17 +1,12 @@
+import { Integer, IntegerFromString } from "$/schema/number";
 import { Config as C, ConfigProvider, Context, Effect, Schema } from "effect";
-import { identity } from "effect/Function";
-import { Integer } from "./schema/number";
 
 const HOST = Schema.Config("HOST", Schema.String).pipe(
 	C.withDefault("0.0.0.0"),
 );
 
-const IntStringSchema = Schema.NumberFromString.pipe(
-	Schema.transform(Integer, { decode: identity, encode: identity }),
-);
-
 const defaultPort = Integer.make(6379);
-const PORT = Schema.Config("PORT", IntStringSchema).pipe(
+const PORT = Schema.Config("PORT", IntegerFromString).pipe(
 	C.withDefault(defaultPort),
 );
 
--- app/schema/number/index.ts
+++ app/schema/number/index.ts
@@ -1,3 +1,7 @@
 import { Schema } from "effect";
 
 export const Integer = Schema.Number.pipe(Schema.int(), Schema.brand("INT"));
+
+export const IntegerFromString = Schema.NumberFromString.pipe(
+	Schema.compose(Integer),
+);
