8cedb89cd0555ad89a8aaf9b0287c249ebf13c04
Evgenii Perminov <evperminov@ozon.ru>
2025-04-13 01:36:07 +0400

    simplify int schema

--- app/schema/number/index.ts
+++ app/schema/number/index.ts
@@ -1,6 +1,6 @@
 import { Schema } from "effect";
 
-export const Integer = Schema.Number.pipe(Schema.int(), Schema.brand("INT"));
+export const Integer = Schema.Int.pipe(Schema.brand("INT"));
 
 export const IntegerFromString = Schema.NumberFromString.pipe(
 	Schema.compose(Integer),
