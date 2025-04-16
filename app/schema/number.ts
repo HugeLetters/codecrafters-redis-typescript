f22d21d9aa932e82ccb64557cdfcfa41f82afaa9
Evgenii Perminov <evperminov@ozon.ru>
2025-04-17 00:24:10 +0400

    add fraction schema

--- app/schema/number.ts
+++ app/schema/number.ts
@@ -1,7 +1,27 @@
 import { Schema } from "effect";
+import { DigitString } from "./string";
 
 export const Integer = Schema.Int.pipe(Schema.brand("INT"));
 
 export const IntegerFromString = Schema.NumberFromString.pipe(
 	Schema.compose(Integer),
+);
+
+export const Fraction = Schema.Number.pipe(
+	Schema.greaterThanOrEqualTo(0),
+	Schema.lessThan(1),
+	Schema.brand("Fraction"),
+);
+
+export const FractionFromDigitString = DigitString.pipe(
+	Schema.transform(Schema.NumberFromString, {
+		decode(x) {
+			return `0.${x}`;
+		},
+		encode(x) {
+			return x.slice(2);
+		},
+		strict: false,
+	}),
+	Schema.compose(Fraction),
 );
