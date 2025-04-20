52b50af146a45b03fe1a31515393934f49ff7fa0
Evgenii Perminov <evperminov@ozon.ru>
2025-04-20 17:55:08 +0200

    implement resp double

--- app/schema/resp/number.ts
+++ app/schema/resp/number.ts
@@ -1,9 +1,10 @@
 import {
 	Integer as Integer_,
 	MultiplierFromImplicitNumberSign,
+	NaN_,
 } from "$/schema/number";
 import { DigitString } from "$/schema/string";
-import { Schema } from "effect";
+import { Schema, flow } from "effect";
 import { CRLF } from "./constants";
 
 const IntegerPrefix = ":";
@@ -30,4 +31,66 @@ export const Integer = Schema.TemplateLiteralParser(
 	}),
 );
 
-export const Double = Schema.Never;
+const NanLiteral = "nan";
+const NanFromString = Schema.Literal(NanLiteral).pipe(
+	Schema.transform(NaN_, {
+		decode() {
+			return Number.NaN;
+		},
+		encode(): typeof NanLiteral {
+			return NanLiteral;
+		},
+	}),
+);
+
+const InfinityFromString = Schema.transformLiterals(
+	["inf", Number.POSITIVE_INFINITY],
+	["-inf", Number.NEGATIVE_INFINITY],
+);
+
+const DoublePrefix = ",";
+export const Double = Schema.TemplateLiteralParser(
+	DoublePrefix,
+	Schema.Union(
+		InfinityFromString,
+		NanFromString,
+		Schema.String.pipe(
+			Schema.pattern(/^[+-]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?$/),
+			Schema.transform(Schema.Number, {
+				decode(s) {
+					return Number.parseFloat(s);
+				},
+				encode(s) {
+					const standard = s.toString();
+					const exponential = toOptimalExponential(s);
+					return exponential.length < standard.length ? exponential : standard;
+				},
+			}),
+			Schema.finite(),
+		),
+	),
+	CRLF,
+).pipe(
+	Schema.transform(Schema.Number, {
+		decode(x) {
+			return x[1];
+		},
+		encode(x) {
+			return [DoublePrefix, x, CRLF] as const;
+		},
+	}),
+);
+
+const toOptimalExponential = flow(
+	(x: number) => x.toExponential(),
+	(x) => {
+		const [base, exponent] = x.split("e");
+		if (!base || !exponent) {
+			return x;
+		}
+
+		const [int, fraction = ""] = base.split(".");
+		const newExponent = Number.parseInt(exponent) - fraction.length;
+		return `${int}${fraction}e${newExponent}`;
+	},
+);
