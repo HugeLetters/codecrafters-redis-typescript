5beda160c0a7e6cd1d8cad6cd90b5e67fb93a53a
Evgenii Perminov <evperminov@ozon.ru>
2025-05-07 15:03:56 +0200

    primitive schema annotations

--- app/schema/resp/primitives.ts
+++ app/schema/resp/primitives.ts
@@ -2,9 +2,16 @@ import { Schema } from "effect";
 import { CRLF } from "./constants";
 
 export const NullPrefix = "_";
-export const PlainNull = Schema.transformLiteral(`${NullPrefix}${CRLF}`, null);
-export const BulkStringNull = Schema.transformLiteral(`$-1${CRLF}`, null);
-export const ArrayNull = Schema.transformLiteral(`*-1${CRLF}`, null);
+export const PlainNull = Schema.transformLiteral(
+	`${NullPrefix}${CRLF}`,
+	null,
+).pipe(Schema.annotations({ identifier: "RespNull" }));
+export const BulkStringNull = Schema.transformLiteral(`$-1${CRLF}`, null).pipe(
+	Schema.annotations({ identifier: "NullBulkString" }),
+);
+export const ArrayNull = Schema.transformLiteral(`*-1${CRLF}`, null).pipe(
+	Schema.annotations({ identifier: "NullArray" }),
+);
 export const Null = Schema.Union(PlainNull, BulkStringNull, ArrayNull);
 
 export const BooleanPrefix = "#";
@@ -15,6 +22,7 @@ export const Boolean_ = Schema.TemplateLiteralParser(
 	Schema.transformLiterals([True, true], [False, false]),
 	CRLF,
 ).pipe(
+	Schema.annotations({ identifier: "RespBoolean" }),
 	Schema.transform(Schema.Boolean, {
 		decode(template) {
 			return template[1];
