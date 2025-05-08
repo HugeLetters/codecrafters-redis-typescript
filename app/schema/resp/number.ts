b893350d4d35d524a399473910dd2cb6ce4dcc8e
Evgenii Perminov <evperminov@ozon.ru>
2025-05-08 22:41:41 +0200

    leftover number schemas

--- app/schema/resp/number.ts
+++ app/schema/resp/number.ts
@@ -1,29 +1,59 @@
-import { Integer as Integer_, NaN_ } from "$/schema/number";
+import { Integer as Integer_, IntegerFromString, NaN_ } from "$/schema/number";
 import { DigitString, ImplicitNumberSign } from "$/schema/string";
-import { BigInt as BigInt_, Schema, flow } from "effect";
+import { Log, parseFail } from "$/schema/utils";
+import { BigInt as BigInt_, Effect, ParseResult, Schema, flow } from "effect";
 import { CRLF } from "./constants";
+import { LeftoverData, noLeftover } from "./leftover";
 
 export const IntegerPrefix = ":";
-export const Integer = Schema.TemplateLiteralParser(
+const LeftoverInteger_ = Schema.TemplateLiteralParser(
 	IntegerPrefix,
 	ImplicitNumberSign,
-	DigitString.pipe(Schema.parseNumber),
-	CRLF,
-).pipe(
-	Schema.annotations({ identifier: "RespInteger" }),
-	Schema.transform(Integer_, {
-		decode(template) {
-			const number = template[2];
+	Schema.String,
+).pipe(Schema.annotations({ identifier: "LeftoverInteger" }));
+
+const IntegerRegex = /^(\d+)\r\n([\s\S]*)$/;
+const parseIntFromString = ParseResult.decode(IntegerFromString);
+export const LeftoverInteger = LeftoverInteger_.pipe(
+	Schema.transformOrFail(LeftoverData(Integer_), {
+		decode: Effect.fn(function* (template, _opts, ast) {
+			const str = template[2];
+			const result = IntegerRegex.exec(str);
+			if (result === null) {
+				const expected = Log.expected(`{integer}${CRLF}{leftover}`);
+				const received = Log.received(str);
+				const message = `Expected string matching: ${expected}. Received ${received}`;
+				return yield* parseFail(ast, str, message);
+			}
+
+			const [_match, digits = "", leftover = ""] = result;
+			const number = yield* parseIntFromString(digits);
 			const sign = template[1] === "+" ? 1 : -1;
-			return sign * number;
-		},
-		encode(integer) {
-			return [
+			const output = sign * number;
+			return { data: output, leftover };
+		}),
+		encode(input) {
+			const integer = input.data;
+			type Output = typeof LeftoverInteger_.Type;
+			const output: Output = [
 				IntegerPrefix,
 				integer < 0 ? "-" : "+",
-				Math.abs(integer),
-				CRLF,
-			] as const;
+				`${Math.abs(integer)}${CRLF}${input.leftover}`,
+			];
+			return ParseResult.succeed(output);
+		},
+		strict: true,
+	}),
+);
+
+export const Integer = LeftoverInteger.pipe(
+	noLeftover((t) => t.leftover, "RespInteger"),
+	Schema.transform(Schema.typeSchema(Integer_), {
+		decode(template) {
+			return template.data;
+		},
+		encode(integer) {
+			return { data: integer, leftover: "" };
 		},
 	}),
 );
@@ -47,53 +77,66 @@ const InfinityFromString = Schema.transformLiterals(
 ).pipe(Schema.annotations({ identifier: "InfinityFromString" }));
 
 export const DoublePrefix = ",";
-export const Double = Schema.TemplateLiteralParser(
+const LeftoverDouble_ = Schema.TemplateLiteralParser(
 	DoublePrefix,
-	Schema.Union(
-		InfinityFromString,
-		NanFromString,
-		Schema.String.pipe(
-			Schema.pattern(/^[+-]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?$/, {
-				identifier: "NumberString",
-			}),
-			Schema.transform(Schema.Number, {
-				decode(s) {
-					return Number.parseFloat(s);
-				},
-				encode(s) {
-					const standard = s.toString();
-					const exponential = toOptimalExponential(s);
-					return exponential.length < standard.length ? exponential : standard;
-				},
-			}),
-			Schema.finite({ identifier: "NumberFromString" }),
-		),
+	Schema.String,
+).pipe(Schema.annotations({ identifier: "LeftoverDouble" }));
+
+const DoubleValue = Schema.Union(
+	InfinityFromString,
+	NanFromString,
+	Schema.String.pipe(
+		Schema.pattern(/^[+-]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?$/, {
+			identifier: "NumberString",
+		}),
+		Schema.transform(Schema.Number, {
+			decode(s) {
+				return Number.parseFloat(s);
+			},
+			encode(s) {
+				const standard = s.toString();
+				const exponential = toOptimalExponential(s);
+				return exponential.length < standard.length ? exponential : standard;
+			},
+		}),
+		Schema.finite({ identifier: "NumberFromString" }),
 	),
-	CRLF,
-).pipe(
-	Schema.annotations({ identifier: "Double" }),
-	Schema.transform(Schema.Number, {
-		decode(x) {
-			return x[1];
-		},
-		encode(x) {
-			return [DoublePrefix, x, CRLF] as const;
+);
+
+const LeftoverDoubleRegex = /^([\s\S]+?)\r\n([\s\S]*)$/;
+export const LeftoverDouble = LeftoverDouble_.pipe(
+	Schema.transformOrFail(LeftoverData(DoubleValue), {
+		decode: Effect.fn(function* (template, _opts, ast) {
+			const input = template[1];
+			const result = LeftoverDoubleRegex.exec(input);
+			if (!result) {
+				const expected = Log.expected(`{content}${CRLF}{leftover}`);
+				const received = Log.received(input);
+				const message = `Expected string matching: ${expected}. Received ${received}`;
+				return yield* parseFail(ast, input, message);
+			}
+
+			const [_match, data = "", leftover = ""] = result;
+			return { data, leftover };
+		}),
+		encode(input) {
+			type Output = typeof LeftoverDouble_.Type;
+			const str = `${input.data}${CRLF}${input.leftover}`;
+			return ParseResult.succeed<Output>([DoublePrefix, str]);
 		},
 	}),
 );
 
-const toOptimalExponential = flow(
-	(x: number) => x.toExponential(),
-	(x) => {
-		const [base, exponent] = x.split("e");
-		if (!base || !exponent) {
-			return x;
-		}
-
-		const [int, fraction = ""] = base.split(".");
-		const newExponent = Number.parseInt(exponent) - fraction.length;
-		return `${int}${fraction}e${newExponent}`;
-	},
+export const Double = LeftoverDouble.pipe(
+	noLeftover((data) => data.leftover, "Double"),
+	Schema.transform(Schema.Number, {
+		decode(data) {
+			return data.data;
+		},
+		encode(data): typeof LeftoverDouble.Type {
+			return { data, leftover: "" };
+		},
+	}),
 );
 
 export const BigNumberPrefix = "(";
@@ -119,4 +162,18 @@ export const BigNumber = Schema.TemplateLiteralParser(
 			] as const;
 		},
 	}),
+);
+
+const toOptimalExponential = flow(
+	(x: number) => x.toExponential(),
+	(x) => {
+		const [base, exponent] = x.split("e");
+		if (!base || !exponent) {
+			return x;
+		}
+
+		const [int, fraction = ""] = base.split(".");
+		const newExponent = Number.parseInt(exponent) - fraction.length;
+		return `${int}${fraction}e${newExponent}`;
+	},
 );
