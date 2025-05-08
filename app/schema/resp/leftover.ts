95db45221f3e7c4c46214d58caced429939dbfc9
Evgenii Perminov <evperminov@ozon.ru>
2025-05-08 03:37:15 +0200

    primitives leftover schemas

--- app/schema/resp/leftover.ts
+++ app/schema/resp/leftover.ts
@@ -1,4 +1,25 @@
+import { Schema } from "effect";
+import { ParseFailLog } from "../utils";
+
 export type LeftoverData<T> = {
 	readonly data: T;
 	readonly leftover: string;
 };
+
+export function noLeftover<TType, TEncoded>(
+	getLeftover: (value: TType) => string,
+	identifier: string,
+) {
+	return Schema.filter<Schema.Schema<TType, TEncoded>>(
+		(data) => {
+			const leftover = getLeftover(data);
+			if (leftover === "") {
+				return;
+			}
+
+			const received = ParseFailLog.received(leftover);
+			return `Leftover data must be empty. Received ${received}`;
+		},
+		{ identifier },
+	);
+}
--- app/schema/resp/primitives.ts
+++ app/schema/resp/primitives.ts
@@ -1,34 +1,63 @@
 import { Schema } from "effect";
 import { CRLF } from "./constants";
+import { noLeftover } from "./leftover";
 
 export const NullPrefix = "_";
 export const PlainNull = Schema.transformLiteral(
 	`${NullPrefix}${CRLF}`,
 	null,
 ).pipe(Schema.annotations({ identifier: "RespNull" }));
+
 export const BulkStringNull = Schema.transformLiteral(`$-1${CRLF}`, null).pipe(
 	Schema.annotations({ identifier: "NullBulkString" }),
 );
+
 export const ArrayNull = Schema.transformLiteral(`*-1${CRLF}`, null).pipe(
 	Schema.annotations({ identifier: "NullArray" }),
 );
+
 export const Null = Schema.Union(PlainNull, BulkStringNull, ArrayNull);
+
+export const LeftoverPlainNull = Schema.TemplateLiteralParser(
+	PlainNull,
+	Schema.String,
+).pipe(Schema.annotations({ identifier: "LeftoverRespNull" }));
+
+export const LeftoverBulkStringNull = Schema.TemplateLiteralParser(
+	BulkStringNull,
+	Schema.String,
+).pipe(Schema.annotations({ identifier: "LeftoverNullBulkString" }));
+
+export const LeftoverArrayNull = Schema.TemplateLiteralParser(
+	ArrayNull,
+	Schema.String,
+).pipe(Schema.annotations({ identifier: "LeftoverNullArray" }));
+
+export const LeftoverNull = Schema.TemplateLiteralParser(
+	Null,
+	Schema.String,
+).pipe(Schema.annotations({ identifier: `Leftover<${Null}>` }));
 
 export const BooleanPrefix = "#";
 const True = "t";
 const False = "f";
-export const Boolean_ = Schema.TemplateLiteralParser(
+export const LeftoverBoolean = Schema.TemplateLiteralParser(
 	BooleanPrefix,
 	Schema.transformLiterals([True, true], [False, false]),
 	CRLF,
-).pipe(
-	Schema.annotations({ identifier: "RespBoolean" }),
+	Schema.String,
+).pipe(Schema.annotations({ identifier: "LeftoverBoolean" }));
+
+export const Boolean_ = LeftoverBoolean.pipe(
+	noLeftover((t) => t[3], "RespBoolean"),
 	Schema.transform(Schema.Boolean, {
 		decode(template) {
 			return template[1];
 		},
 		encode(bool) {
-			return [BooleanPrefix, bool, CRLF] as const;
+			type Result = typeof LeftoverBoolean.Type;
+			const result: Result = [BooleanPrefix, bool, CRLF, ""];
+			return result;
 		},
 	}),
 );
