bd112e396c2bbb190556e4ad27517b5aa3976b77
Evgenii Perminov <evperminov@ozon.ru>
2025-05-09 18:32:01 +0200

    fix exports

--- app/schema/resp/index.export.ts
+++ app/schema/resp/index.export.ts
@@ -1,4 +1,4 @@
 export { Array_ as Array } from "./container/array";
-export { Number } from "./number";
+export { Number_ as Number } from "./number";
 export { Primitive } from "./primitive";
-export { String } from "./string";
+export { String_ as String } from "./string";
