88cf0242c97d498d2846976a795f9022e6b318c3
Evgenii Perminov <evperminov@ozon.ru>
2025-05-09 18:32:34 +0200

    add missing exports

--- app/schema/resp/index.export.ts
+++ app/schema/resp/index.export.ts
@@ -1,4 +1,5 @@
-export { Array_ as Array } from "./container/array";
+export { Array_ as Array, ArrayPrefix } from "./container/array";
+export { Error_ as Error } from "./error";
 export { Number_ as Number } from "./number";
 export { Primitive } from "./primitive";
 export { String_ as String } from "./string";
