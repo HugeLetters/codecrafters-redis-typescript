f410188223d8f6718c8d8bb276301c1b2c698b88
Evgenii Perminov <evperminov@ozon.ru>
2025-05-17 19:56:12 +0200

    add main resp export

--- app/schema/resp/index.export.ts
+++ app/schema/resp/index.export.ts
@@ -9,6 +9,7 @@ export {
 } from "./container/prefix";
 export { Set_ as Set } from "./container/set";
 export { Error_ as Error } from "./error";
+export { RespData, type RespValue } from "./main";
 export { Number_ as Number } from "./number";
 export { Primitive } from "./primitive";
 export { String_ as String } from "./string";
