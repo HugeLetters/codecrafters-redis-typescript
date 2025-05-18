3ea48025743ea2aaddb9b4c9704f7350511f7a71
Evgenii Perminov <evperminov@ozon.ru>
2025-05-18 16:42:21 +0200

    handle incoming messages

--- app/main.ts
+++ app/main.ts
@@ -1,30 +1,48 @@
 import { BunRuntime } from "@effect/platform-bun";
-import { Effect, Stream } from "effect";
+import { Effect, Match, Schema, Stream } from "effect";
+import type { Socket } from "node:net";
 import { provideConfigService } from "./config";
+import { Resp } from "./schema/resp";
 import { acquireSocketStream } from "./server";
 import { getSocketDataStream, getSocketWriter } from "./server/socket";
 
 const main = Effect.gen(function* () {
 	const socketStream = yield* acquireSocketStream;
 	const socketHandlerStream = socketStream.pipe(
-		Stream.mapEffect(
-			(c) => {
-				const write = getSocketWriter(c);
-				const dataStream = getSocketDataStream(c);
-				return dataStream.pipe(
-					Stream.map((b) => b.toString("utf8")),
-					Stream.tap((data) => Effect.log("Received", data)),
-					Stream.mapEffect((_data) => write("+PONG\r\n")),
-					Stream.catchTag("SocketWrite", () => Effect.void),
-					Stream.runDrain,
-					Effect.tap(Effect.log("Disconnected")),
-				);
-			},
-			{ concurrency: "unbounded" },
-		),
+		Stream.mapEffect(handleSocket, { concurrency: "unbounded" }),
 	);
 
 	return yield* Stream.runDrain(socketHandlerStream);
 });
+
+const decodeResp = Schema.decode(Resp.RespData);
+const encodeResp = Schema.encode(Resp.RespData);
+const decodeRespBuffer = Effect.fn(function* (buffer: Buffer) {
+	const str = buffer.toString("utf8");
+	yield* Effect.logInfo("Received", str);
+	const decoded = yield* decodeResp(str);
+	yield* Effect.logInfo("Decoded", str, "to", decoded);
+	return decoded;
+});
+
+function handleSocket(s: Socket): Effect.Effect<void, never, never> {
+	const write = getSocketWriter(s);
+	const dataStream = getSocketDataStream(s);
+	return dataStream.pipe(
+		Stream.mapEffect(decodeRespBuffer),
+		Stream.map(getCommandResponse),
+		Stream.mapEffect(encodeResp),
+		Stream.mapEffect(write),
+		Stream.catchAll((err) => Effect.logError(err)),
+		Stream.runDrain,
+		Effect.tap(Effect.log("Disconnected")),
+	);
+}
+
+const getCommandResponse = Match.type<Resp.RespValue>().pipe(
+	Match.when(["PING"], () => "PONG"),
+	Match.when(["ECHO", Match.string], ([, message]) => message),
+	Match.orElse((_value) => new Resp.Error({ message: "Unrecognized command" })),
+);
 
 main.pipe(provideConfigService(), Effect.scoped, BunRuntime.runMain);
