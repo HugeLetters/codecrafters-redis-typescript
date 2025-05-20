1d68f5a6502f0c3a4d4ef746e1d07634f565ee6c
Evgenii Perminov <evperminov@ozon.ru>
2025-05-20 19:57:59 +0200

    add flatmap error util

--- app/utils/effect.ts
+++ app/utils/effect.ts
@@ -1,4 +1,4 @@
-import type { Effect, Utils } from "effect";
+import { Effect, Function as Fn, type Utils } from "effect";
 
 export type EffectGen<
 	TEffect extends Effect.Effect<unknown, unknown, unknown>,
@@ -13,3 +13,19 @@ export type EffectGen<
 	Effect.Effect.Success<TEffect>,
 	never
 >;
+
+export const flatMapError = Fn.dual<
+	<E1, A2, E2, R2>(
+		f: (a: E1) => Effect.Effect<A2, E2, R2>,
+	) => <A1, R1>(
+		self: Effect.Effect<A1, E1, R1>,
+	) => Effect.Effect<A1, A2 | E2, R1 | R2>,
+	<A1, E1, R1, A2, E2, R2>(
+		self: Effect.Effect<A1, E1, R1>,
+		f: (a: E1) => Effect.Effect<A2, E2, R2>,
+	) => Effect.Effect<A1, A2 | E2, R1 | R2>
+>(2, (self, map) => {
+	return self.pipe(
+		Effect.catchAll((err) => map(err).pipe(Effect.flatMap(Effect.fail))),
+	);
+});
