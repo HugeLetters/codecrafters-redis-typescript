f8086eefc0d46c2e97114654ba81075e1468217c
Evgenii Perminov <evperminov@ozon.ru>
2025-06-14 19:42:47 +0200

    refactor stdouremove stdout namespace, use reexport

---
+++ app/utils/stdout/main.ts
@@ -0,0 +1,13 @@
+import { color } from "bun";
+
+function ansiColor(colorName: string) {
+	return color(colorName, "ansi");
+}
+
+export function colored(color: string, message: unknown) {
+	return `${ansiColor(color)}${message}\x1b[0m`;
+}
+
+export function green(message: unknown) {
+	return colored("limegreen", message);
+}
