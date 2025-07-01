0cfd929cd8c316e9cbb621556be6b692515c06de
Evgenii Perminov <evperminov@ozon.ru>
2025-07-01 22:02:13 +0200

    fix runtime config parsing

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,11 +1,13 @@
 import { Effect, HashMap, Record } from "effect";
 import { Config } from "./config";
+import { argvConfigProvider } from "$/utils/config";
 
 export class RuntimeConfig extends Effect.Service<RuntimeConfig>()(
 	"RuntimeConfig",
 	{
 		effect: Effect.gen(function* () {
 			const config = yield* Config.pipe(
+				Effect.withConfigProvider(argvConfigProvider()),
 				Effect.map(Record.toEntries),
 				Effect.map(HashMap.fromIterable<string, string>),
 			);
