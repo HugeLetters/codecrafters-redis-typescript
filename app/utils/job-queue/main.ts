1e6b7b739e8965f05b4f8a6f600235691eaad8ce
Evgenii Perminov <evperminov@ozon.ru>
2025-12-03 14:28:40 -0300

    cleanup job queue

--- app/utils/job-queue/main.ts
+++ app/utils/job-queue/main.ts
@@ -2,20 +2,16 @@ import * as Data from "effect/Data";
 import * as Effect from "effect/Effect";
 import * as Iterable from "effect/Iterable";
 import * as Queue from "effect/Queue";
+import * as Schedule from "effect/Schedule";
 import type { Integer } from "$/schema/number";
 
 export type Capacity = Integer;
 export type Job<R = never> = Effect.Effect<void, never, R>;
-class JobQueue<R> extends Data.TaggedClass("JobQueue") {
-	readonly capacity;
-	readonly queue;
 
-	constructor(capacity: Capacity, queue: Queue.Queue<Job<R>>) {
-		super();
-		this.capacity = capacity;
-		this.queue = queue;
-	}
-}
+class JobQueue<R> extends Data.TaggedClass("JobQueue")<{
+	readonly capacity: Capacity;
+	readonly queue: Queue.Queue<Job<R>>;
+}> {}
 
 export type { JobQueue };
 
@@ -24,19 +20,17 @@ export type { JobQueue };
  */
 export const make = Effect.fn(function* <R = never>(capacity: Capacity) {
 	const queue = yield* Queue.bounded<Job<R>>(capacity);
-
-	const job = Effect.gen(function* () {
-		while (true) {
-			yield* Queue.take(queue).pipe(Effect.flatten);
-		}
-	});
+	const job = Effect.repeat(
+		Queue.take(queue).pipe(Effect.flatten),
+		Schedule.forever,
+	);
 	const fibers = Iterable.makeBy(() => job, { length: capacity });
 
-	yield* Effect.all(fibers, { concurrency: capacity }).pipe(Effect.fork);
+	yield* Effect.all(fibers, { concurrency: "unbounded" }).pipe(Effect.fork);
 
-	return new JobQueue(capacity, queue);
+	return new JobQueue({ capacity, queue });
 });
 
-export const offer = Effect.fn(function* <R>(queue: JobQueue<R>, job: Job<R>) {
-	return yield* Queue.offer(queue.queue, job);
+export const offer = Effect.fn(function <R>(queue: JobQueue<R>, job: Job<R>) {
+	return Queue.offer(queue.queue, job);
 });
