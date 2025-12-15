58583c6239168b443428d5fdea2c3b54c645cec1
Evgenii Perminov <evperminov@ozon.ru>
2025-12-14 21:58:52 -0300

    replace integer with bigint

--- app/resp/rdb/type.ts
+++ app/resp/rdb/type.ts
@@ -1,11 +1,10 @@
 import type * as HashMap from "effect/HashMap";
 import type * as HashSet from "effect/HashSet";
 import type * as SortedSet from "effect/SortedSet";
-import type { Integer } from "$/schema/number";
 import type { Satisfies } from "$/utils/type";
 import type { RDBValueType } from "./constants";
 
-type StringEncoded = string | Integer;
+export type StringEncoded = string | bigint;
 
 type RDBList = ReadonlyArray<StringEncoded>;
 type RDBSet = HashSet.HashSet<StringEncoded>;
@@ -17,13 +16,13 @@ interface RDBSortedSetValue {
 type RDBSortedSet = SortedSet.SortedSet<RDBSortedSetValue>;
 
 type RDBHash = HashMap.HashMap<string, StringEncoded>;
-type IntSet = SortedSet.SortedSet<Integer>;
+type IntSet = SortedSet.SortedSet<bigint>;
 
 type Value = StringEncoded | RDBList | RDBSet | RDBSortedSet | RDBHash | IntSet;
 
 interface ValueWithMeta {
 	readonly value: Value;
-	readonly expiry: Integer | undefined;
+	readonly expiry: bigint | undefined;
 }
 
 type RDBDatabase = HashMap.HashMap<string, ValueWithMeta>;
@@ -31,9 +30,9 @@ type RDBDatabase = HashMap.HashMap<string, ValueWithMeta>;
 export type AuxiliaryFields = HashMap.HashMap<string, StringEncoded>;
 
 interface RDBFile {
-	readonly version: Integer;
+	readonly version: bigint;
 	readonly meta: AuxiliaryFields;
-	readonly databases: HashMap.HashMap<Integer, RDBDatabase>;
+	readonly databases: HashMap.HashMap<bigint, RDBDatabase>;
 }
 
 type RDBValueTypeToValueMapper = Satisfies<
