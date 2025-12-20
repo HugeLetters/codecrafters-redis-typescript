bea2c34dcd52423feadac72fd9e28326d942bf37
Evgenii Perminov <evperminov@ozon.ru>
2025-12-20 08:41:25 -0300

    add get known command to app config

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,7 +1,7 @@
 import * as Effect from "effect/Effect";
+import { pipe } from "effect/Function";
 import * as HashMap from "effect/HashMap";
 import * as Record from "effect/Record";
-
 import { argvConfigProvider } from "$/utils/config/argv";
 import { Config } from "./config";
 
@@ -11,13 +11,22 @@ export class AppConfig extends Effect.Service<AppConfig>()(
 		effect: Effect.gen(function* () {
 			const config = yield* Config.pipe(
 				Effect.withConfigProvider(argvConfigProvider()),
-				Effect.map((c) => Record.toEntries(c)),
-				Effect.map((r) => HashMap.fromIterable<string, string>(r)),
 			);
 
+			const configMap = pipe(
+				config,
+				Record.toEntries,
+				HashMap.fromIterable<string, string>,
+			);
+
+			function get(key: string) {
+				return HashMap.get(configMap, key);
+			}
+
 			return {
-				get(key: string) {
-					return HashMap.get(config, key);
+				get,
+				getKnown<K extends keyof typeof config>(key: K) {
+					return config[key];
 				},
 			};
 		}),
