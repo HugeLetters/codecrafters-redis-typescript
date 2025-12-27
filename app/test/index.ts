91c89baa57d4db559ee31ae02ea00c03d7c39169
Evgenii Perminov <evperminov@ozon.ru>
2025-12-26 22:36:04 -0300

    use differ for equivalence in tests

--- app/test/index.ts
+++ app/test/index.ts
@@ -3,7 +3,7 @@ import { fail } from "node:assert/strict";
 import * as Effect from "effect/Effect";
 import * as Equal from "effect/Equal";
 import * as Fn from "effect/Function";
-
+import { UnknownDiffer } from "$/utils/differ";
 import { Stdout } from "$/utils/stdout";
 
 export { test } from "./effect-buntest";
@@ -18,7 +18,10 @@ const unexpectedSuccess = Fn.flow(
 	(m) => fail(m),
 );
 
-export function expectEquivalence<T>(self: T, that: T) {
-	const message = `Expected ${Stdout.colored("red", self)} to equal ${Stdout.green(that)}.`;
-	return expect(Equal.equals(self, that), message).toBeTrue();
+export function expectEquivalence<T>(received: T, expected: T) {
+	const patch = UnknownDiffer.differ.diff(expected, received);
+	const diff = UnknownDiffer.Formatter.format(patch);
+
+	const message = `Expected ${Stdout.colored("red", received)} to equal ${Stdout.green(expected)}.\n${diff}`;
+	return expect(Equal.equals(received, expected), message).toBeTrue();
 }
