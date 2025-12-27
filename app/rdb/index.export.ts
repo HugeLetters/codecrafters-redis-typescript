4642784f639eab68b01a0cba287572f1cd96c3cc
Evgenii Perminov <evperminov@ozon.ru>
2025-12-27 20:07:49 -0300

    add rdb format export

--- app/rdb/index.export.ts
+++ app/rdb/index.export.ts
@@ -1,3 +1,4 @@
 export { DecodeError, decode, decodeFile } from "./decode";
 export { EncodeError, encode } from "./encode";
+export { format } from "./helpers";
 export type { Value, ValueWithMeta } from "./type";
